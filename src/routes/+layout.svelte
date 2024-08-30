64937fff7727e26005cf7d29714440c94eb253cc
Fabrizio Giffi <fagi@solute.de>
2024-08-30 17:16:20 +0200

    app: Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '../app.css';
+	import '@fortawesome/fontawesome-free/css/all.min.css';
+</script>
+
+<main class="max-w-screen-lg mx-auto">
+	<nav class="py-5 flex items-center justify-between">
+		<div class="flex gap-2 items-baseline">
+			<h1 class="text-2xl">DerDieDas</h1>
+			<span class="self-center">//</span>
+			<p>Answering the real questions</p>
+		</div>
+		<a href="/">
+			<i class="fa-solid fa-house"> </i>
+		</a>
+	</nav>
+
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,105 @@
+<script>
+	import capitalize from 'capitalize';
+
+	const GENDERMAP = {
+		m: { tag: 'Maskulin', article: 'der' },
+		f: { tag: 'Feminin', article: 'die' },
+		n: { tag: 'Neutral', article: 'das' }
+	};
+
+	$: qWord = '';
+	$: gender = '';
+	$: errMsg = '';
+
+	async function search(event) {
+		// Reset states
+		errMsg = '';
+		gender = '';
+
+		const data = new FormData(event.currentTarget);
+		qWord = capitalize(data.get('w'));
+
+		const qProps = '&prop=extracts&explaintext&exchars=100&format=json&origin=*';
+		const response = await fetch(
+			`https://de.wiktionary.org/w/api.php?action=query&titles=${qWord}${qProps}`
+		);
+		const body = await response.json();
+		// No results
+		if (body.query.pages['-1']) {
+			errMsg = 'No results found. Have you entered more than a word or special characters?';
+			return;
+		}
+		// Text extract from Wiktionary response
+		const text = await Object.values(body.query.pages)[0].extract;
+		// Results found but invalid return body. Most probably plural forms of a word
+		if (text.includes('=== Deklinierte Form ===')) {
+			errMsg = 'Invalid query. Have you entered the plural form of the word?';
+			return;
+		}
+		// Parse the text to get the gender
+		gender = text.split('Substantiv, ')[1][0];
+	}
+</script>
+
+<form
+	class="flex flex-col gap-2 items-center my-5 mx-auto w-3/5"
+	on:submit|preventDefault={(event) => search(event)}
+>
+	<div class="flex bg-white border rounded-md w-full">
+		<input
+			class="w-11/12 py-2 pl-4 field"
+			type="text"
+			name="w"
+			value={qWord}
+			placeholder="Type a word to get the gender reveal"
+			required
+		/>
+		<button
+			type="submit"
+			class="icon bg-zinc-800 p-1 w-1/12 rounded-md hover:scale-110 hover:border hover:border-white"
+		>
+			<i class="fa fa-search" />
+		</button>
+	</div>
+	<small class="flex gap-2 items-center w-full">
+		<i class="fas fa-info-circle mr-1 text-white"></i>
+		<span> Input field is case-insensitive </span>
+		<span class="mx-5"> — </span>
+		<span> Ä ä Ö ö Ü ü ß allowed</span>
+	</small>
+</form>
+
+{#if errMsg}
+	<div
+		class="flex items-center justify-center w-3/5 mx-auto gap-3 py-4 px-6 border text-red-200 border-red-200 rounded-md"
+	>
+		<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
+		<p>{errMsg}</p>
+	</div>
+{/if}
+
+{#if gender}
+	<section class="rounded-md border border-white py-4 px-6 w-3/5 mx-auto flex justify-between">
+		<div>
+			<h3 class="text-4xl mb-2">{GENDERMAP[gender].article}</h3>
+			<h4 class="text-2xl mb-1">{qWord}</h4>
+			<p>Substantiv, {GENDERMAP[gender].tag}</p>
+		</div>
+		<div class="flex flex-col text-end">
+			<a
+				class="uppercase hover:underline hover:decoration-2 text-violet-300"
+				target="_blank"
+				href={`https://www.duden.de/suchen/dudenonline/${qWord}`}
+			>
+				definition - duden
+			</a>
+			<a
+				class="uppercase hover:underline hover:decoration-2 text-violet-300"
+				target="_blank"
+				href={`https://www.deepl.com/de/translator#de/en-us/${qWord}`}
+			>
+				translation - deepl
+			</a>
+		</div>
+	</section>
+{/if}
